70e1503ed182f802724f11bff3cc2e9e7a3c0376
dpavam <hhz161@qmul.ac.uk>
2024-10-16 16:38:51 +0100

    feat: adds a new endpoint with EFO traits grouped by parent EFO id

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import asyncio
+import logging
+from typing import List, Dict
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-import logging
-# import requests
 import httpx
-import asyncio
+from structures import GwasCatalogCategories
+
 
 # Set logger
 logger = logging.getLogger(__name__)
@@ -22,12 +24,15 @@ app.add_middleware(
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
 )
+
+
 @app.get("/")
 def welcome_page():
     return "GWAS catalog proxy server"
 
+
 @app.get("/api/traits/{gene_id}")
-async def fetch_data(gene_id: str):
+async def fetch_trait_data(gene_id: str):
     """Fetches trait data for the specified HUMAN gene from GWAS catalog"""
     url = f"https://www.ebi.ac.uk/gwas/api/v2/genes/{gene_id}/traits?size=5000"
     # r = requests.get(url)
@@ -35,43 +40,37 @@ async def fetch_data(gene_id: str):
         r = await client.get(url)
         if r.status_code == 200:
             data = r.json()
-            l = data['page']['totalElements']
-            traits = extract_traits(data, l)
-            return unique_traits(traits)
+            length = data["page"]["totalElements"]
+            traits = extract_traits(data, length)
+            ind_traits = individual_traits(traits)
+            return unique_traits(ind_traits)
         else:
-            raise HTTPException(status_code=r.status_code, detail="Failed to fetch data")
+            raise HTTPException(
+                status_code=r.status_code, detail="Failed to fetch data"
+            )
 
-# For now
-# TODO: Create a new endpoint to retrieve the parent traits of each associated term: ex: https://www.ebi.ac.uk/gwas/rest/api/parentMapping/EFO_0006336
-# Look at the "parent" value, it contains the parent category by which the trait can be grouped. 
-# Perhaps we can send something like:
-    """
-    {
-        "parent": [
-        {"key":"label"},
-        {"key":"label"},
-        {"key":"label"}
-        ],
-        "parent2: [
-        ]
-    }
-    """
 
-# Then we can unpack that at the client side and use the existing diagrams with relevant icons to display the HUMAN categories
-
-# Check what the content is and construct the dictionary
 @app.get("/api/traits/parents/{gene_id}")
 async def parent_data_mappings(gene_id: str):
     """Fetches parent data for each EFO from a gene"""
     # Fetch EFOs for a given gene
-    traits = await fetch_data(gene_id)
+    traits = await _fetch_data(gene_id)
     # Fetch the parents for each efo trait
-    parent_tasks = [fetch_parent_data(trait["key"]) for trait in traits]
+    parent_tasks = [_fetch_parent_data(trait["key"]) for trait in traits]
     data = await asyncio.gather(*parent_tasks)
-    return data
+
+    # Correct parent uri
+    parent_id_corrected_data = extract_parent_efo(data)
+    # return parent_id_corrected_data
+
+    # Return grouped data
+    grouped_data = group_by_category(parent_id_corrected_data)
+    return grouped_data
+    # TODO: this works but now we need the above structure to pass into the website.
+
 
 # Helpers
-async def fetch_data(gene_id: str):
+async def _fetch_data(gene_id: str):
     """Fetches trait data for the specified HUMAN gene from GWAS catalog"""
     url = f"https://www.ebi.ac.uk/gwas/api/v2/genes/{gene_id}/traits?size=5000"
     # r = requests.get(url)
@@ -79,16 +78,20 @@ async def fetch_data(gene_id: str):
         r = await client.get(url)
         if r.status_code == 200:
             data = r.json()
-            l = data['page']['totalElements']
-            traits = extract_traits(data, l)
-            return unique_traits(traits)
+            length = data["page"]["totalElements"]
+            traits = extract_traits(data, length)
+            ind_traits = individual_traits(traits)
+            return unique_traits(ind_traits)
         else:
-            raise HTTPException(status_code=r.status_code, detail="Failed to fetch data")
+            raise HTTPException(
+                status_code=r.status_code, detail="Failed to fetch data"
+            )
 
-async def fetch_parent_data(trait_id: str):
+
+async def _fetch_parent_data(trait_id: str):
     """Fetches parent trait data for a specified EFO term"""
     url = f"https://www.ebi.ac.uk/gwas/rest/api/parentMapping/{trait_id}"
-    async with httpx.AsyncClient() as client:
+    async with httpx.AsyncClient(timeout=30) as client:
         r = await client.get(url)
         if r.status_code == 200:
             data = r.json()
@@ -97,25 +100,65 @@ async def fetch_parent_data(trait_id: str):
                 "trait_id": trait_id,
                 "trait": data.get("trait", None),
                 "parent": data.get("parent", None),
-                "colourLabel": data.get("colourLabel", None)
+                "parent_id": data.get("parentUri", None),
+                "colourLabel": data.get("colourLabel", None),
             }
             return trait_data
         else:
-            raise HTTPException(status_code=r.status_code, detail="Failed to fetch data")
+            raise HTTPException(
+                status_code=r.status_code, detail="Failed to fetch data"
+            )
+
 
 def extract_traits(data, length=LENGTH):
-    """ Parses the GWAS catalog API response to extract the EFOs and their labels"""
+    """Parses the GWAS catalog API response to extract the EFOs and their labels"""
     efo_list = []
     for i in range(0, length):
         efos = data["_embedded"]["efos"][i]["efoTraits"]
         efo_list.append(efos)
     return efo_list
 
-def unique_traits(efo_list):
-    """ Loops through the extract_traits result to keep unique entries only"""
+
+def individual_traits(efo_list):
+    """Loops through the extract_traits result to make entries individual"""
     mas_list = []
     for l1 in efo_list:
         for l2 in l1:
             mas_list.append(l2)
             # handle the duplicates on client side.
+            # Handle duplicates now
+
     return mas_list
+
+
+def unique_traits(ind_traits):
+    """Removes duplicates from individual_traits"""
+    return list({v["key"]: v for v in ind_traits}.values())
+
+
+def group_by_category(parent_data) -> List[GwasCatalogCategories]:
+    """Returns a list[TypedDict] with parent_efos:[{efos}]"""
+    parent_dict: GwasCatalogCategories = {}
+    for i in parent_data:
+        parent_id = i.get("parent_id", None)
+        if parent_id not in parent_dict:
+            parent_dict[parent_id] = []
+        parent_dict[parent_id].append(i)
+    return [parent_dict]
+
+
+def extract_parent_efo(efo_data) -> Dict | None:
+    """Replaces ParentURIs with parent's EFO id"""
+    # Let's keep things inmutable
+    copy_efo_data = efo_data.copy()
+    for i in copy_efo_data:
+        i["parent_id"] = _get_parent_efo(i["parent_id"])
+    return copy_efo_data
+
+
+def _get_parent_efo(parent_uri) -> str | None:
+    """Splits a URI to extract the EFO ID"""
+    efo = parent_uri.split("/")[-1]
+    if efo.startswith("EFO"):
+        return efo
+    return None
---
+++ structures.py
@@ -0,0 +1,30 @@
+from typing import TypedDict, List, Optional
+from typing_extensions import NotRequired
+
+class GwasCatalogCategories(TypedDict):
+    """Typed dict structure to store EFO data by parent id"""
+    EFO_0000405: NotRequired[List]
+    EFO_0004324: NotRequired[List]
+    EFO_0004529: NotRequired[List]
+    EFO_0004503: NotRequired[List]
+    EFO_0000540: NotRequired[List]
+    EFO_0000589: NotRequired[List]
+    EFO_0000001: NotRequired[List]
+    EFO_0000408: NotRequired[List]
+    GO_0008150: NotRequired[List]
+    EFO_0004298: NotRequired[List]
+    EFO_0000319: NotRequired[List]
+    EFO_0000618: NotRequired[List]
+    EFO_0001444: NotRequired[List]
+    EFO_0000616: NotRequired[List]
+    EFO_0004872: NotRequired[List]
+    EFO_0004582: NotRequired[List]
+    EFO_0004732: NotRequired[List]
+
+
+# Current parent efo_ids 
+"""
+['EFO_0000405', 'EFO_0004324', 'EFO_0004529', 'EFO_0004503', 'EFO_0000540', 'EFO_0000589',
+ 'EFO_0000001', 'EFO_0000408', 'GO_0008150', 'EFO_0004298', 'EFO_0000319', 'EFO_0000618',
+ 'EFO_0001444', 'EFO_0000616', 'EFO_0004872', 'EFO_0004582', 'EFO_0004732']
+ """
